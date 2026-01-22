b9f3129fc487466086efb90cc535fa15e142a1ab
Shreeya <shreeyajaiswal2006@gmail.com>
2026-01-23 03:35:30 +0530

    feat: add custom React implementation with mainContainer function and index.html

---
+++ customReact/customReact.js
@@ -0,0 +1,21 @@
+function mainContainer(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    container.appendChild(domElement)
+}
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "http://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+} //an obj. this is.
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer) 
