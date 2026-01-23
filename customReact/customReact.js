b6c6d3bfeebc42c9a615ef3ac3cf2b132951b92b
Shreeya <shreeyajaiswal2006@gmail.com>
2026-01-24 01:05:13 +0530

    react jsx

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,9 +1,19 @@
-function mainContainer(reactElement, container){
+function customRender(reactElement, container){
+    /*
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
     container.appendChild(domElement)
+    */
+   const domElement = document.createElement(reactElement.type)
+   domElement.innerHTML = reactElement.children
+   for (const prop in reactElement.props) {
+    if(prop === 'children') continue
+    domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+
 }
 
 
@@ -18,4 +28,4 @@ const reactElement = {
 
 const mainContainer = document.querySelector('#root')
 
-customRender(reactElement, mainContainer) 
+customRender(reactElement, mainContainer) //reactElement se utha kar mainContainer me dalna hai
