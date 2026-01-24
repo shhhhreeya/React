b928dfea6aedc3d73211a323e9c9a8058a051571
Shreeya <shreeyajaiswal2006@gmail.com>
2026-01-24 13:46:10 +0530

    feat: add custom React elements and render logic in main.jsx

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "http://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
 
-    <App />
+function Myapp(){
+    return(
+        <div>
+            <h1>Custom react app</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target = '_blank'>Visit Google
+    </a>
+)
+
+const areactelement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: "_blank"},
+    'Click here to visit Google!'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    areactelement
 
 )
